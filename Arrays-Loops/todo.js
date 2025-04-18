49858d06babbde37b96fdaeeda1b155eb2cf688a
vineelkrish <vineelnaraboina.2017@gmail.com>
2025-04-18 12:37:12 +0530

    TODO JS

---
+++ Arrays-Loops/todo.js
@@ -0,0 +1,22 @@
+ const todo=[];
+ renderTodo();
+ function renderTodo(){
+    let todoHTML='';
+ for(let i=0;i<todo.length;i++)
+ {
+    const td=todo[i];
+    const html=`<p>${td}</p>`;
+    todoHTML+=html;
+ }
+ document.querySelector('.todo-list').innerHTML=todoHTML;
+  }
+ 
+ function addTodo() {
+    const ele=document.querySelector('.js-input');
+    const name=ele.value;
+    todo.push(name);
+    console.log(todo);
+
+    ele.value='';
+    renderTodo();
+ }
